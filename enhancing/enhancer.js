0a932649983adf71bcca9a1ca05e510a48205455
Daniel Hermansen <daniel.j.hermansen@gmail.com>
2020-06-11 20:44:16 -0600

    MVP complete

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -5,16 +5,29 @@ module.exports = {
   get,
 };
 
-function succeed(item) {
-  return { ...item };
+function succeed(name, enhancement, durability) {
+  let newItem = { name, enhancement, durability };
+  if (enhancement < 20) {
+    return { ...newItem, enhancement: enhancement + 1 };
+  } else if (enhancement === 20){
+    return newItem
+  }
 }
 
-function fail(item) {
-  return { ...item };
+function fail(name, enhancement, durability) {
+  let newItem = { name, enhancement, durability };
+  if(enhancement < 15) {
+    return { ...newItem, durability: durability - 5 };
+  } else if (enhancement >= 15) {
+    return { ...newItem, durability: durability - 10 };
+  } else if (enhancement >= 17) {
+    return { ...newItem, enhancement: enhancement - 1, durability: durability - 10 };
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  const newItem = {...item, durability: 100};
+  return newItem;
 }
 
 function get(item) {
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,61 @@
-const enhancer = require('./enhancer.js');
+const { succeed, fail, repair } = require('./enhancer.js');
 // test away!
+
+it('should run the tests', function() {
+    expect(true).toBe(true);
+});
+
+
+describe('enhancer.js', function() {
+
+    describe('succeed()', function(){
+        it('should return increase enhancement by 1 when enhancement is < 20', function() {
+            expect(succeed("Master Sword", 19, 95 )).toEqual({
+                name: "Master Sword",
+                enhancement: 20,
+                durability: 95,
+            })
+        })
+
+        it('should have no changes when enhancement level equals 20', function() {
+            expect(succeed("Biggoron's Sword", 20, 60)).toEqual({
+                name: "Biggoron's Sword", 
+                enhancement: 20,
+                durability: 60,
+            })
+        })
+    });
+
+    describe('fail()', function() {
+        it('should decrease durability by 5 when enhancement is < 15', function() {
+            expect(fail("Mirror Shield", 13, 85 )).toMatchObject({
+              durability: 80,
+            });
+        });
+        it('should decrease durability by 10 when enhancement is >= 15', function() {
+            expect(fail("Mirror Shield", 16, 85 )).toEqual({
+                name: "Mirror Shield",
+                enhancement: 16,
+                durability: 75,
+            });
+        });
+        // it('should decrease enhancement by 1 when enhancement is > 16', function() {
+        //     expect(fail("Mirror Shield", 18, 85 )).toMatchObject({
+        //         name: "Mirror Shield",
+        //         enhancement: 18,
+        //         durability: 75,
+        //     });
+        // });
+    });
+
+    describe('repair()', function() {
+        it('passing item as an argument should return a new item with durability at 100', function() {
+            expect(repair({ name: "Hylian Shield", enhancement: 15})).toStrictEqual({
+                name: "Hylian Shield",
+                enhancement: 15,
+                durability: 100,
+            });
+        });
+
+    });
+});
